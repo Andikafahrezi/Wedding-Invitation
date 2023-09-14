243c08c9f99497f8edee2d1f4807f68725570ef0
dika <dika@addust.co.id>
2023-09-14 22:24:18 +0700

    update link

--- W1-1.js
+++ W1-1.js
@@ -5,7 +5,6 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-
 // Klik di luar elemen
 const hm = document.querySelector("#hamburger-menu");
 
@@ -13,5 +12,25 @@ document.addEventListener("click", function (e) {
   if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+});
 
+window.addEventListener("load", function () {
+  const form = document.getElementById("form-WID");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Konfirmasi Pesanan Berhasil!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    });
+  });
 });
